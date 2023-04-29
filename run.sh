f5d3df8d1b064be223d79613bd05201611284e6d
Avinash Pai <paiavinash200@gmail.com>
2023-04-29 15:53:23 -0700

    Small updates to run.sh
    - run the flask commands directly
    - cd to correct dirs for commands

--- run.sh
+++ run.sh
@@ -3,9 +3,10 @@
 cd ./backend
 poetry shell
 #!/usr/bin/bash
-cd ../client
-npm run start-api &
+cd ..
+flask --app backend run --debug &
 pid[0]=$!
+cd ./client
 npm start &
 pid[1]=$!
 trap "kill ${pid[0]} ${pid[1]}; exit 1" INT
